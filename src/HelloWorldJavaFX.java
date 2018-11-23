3317812c67fc6f762c0ccf3c002a4ba605b3efbe
Adrielle Costa <adrielle.gasparc@gmail.com>
2018-11-23 17:05:20 -0300

    Adicionando cores e posicionamento às formas

--- src/HelloWorldJavaFX.java
+++ src/HelloWorldJavaFX.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
@@ -12,9 +13,13 @@ public class HelloWorldJavaFX extends Application{
     @Override
     public void start(Stage palco){
         //Personagem
-        Rectangle ceu = new Rectangle(900, 450);
-        Rectangle mar = new Rectangle(900, 450);
-        Circle sol = new Circle(90);
+        Rectangle ceu = new Rectangle(900, 450, Color.ORANGERED);
+        Rectangle mar = new Rectangle(900, 450, Color.DARKBLUE);
+        Circle sol = new Circle(90, Color.YELLOW);
+        //Posicionamento
+        ceu.setTranslateY(-200);
+        mar.setTranslateY(200);
+        sol.setTranslateY(0);      
         //Historia
         StackPane historia = new StackPane();
         historia.getChildren().addAll(ceu, sol, mar);
