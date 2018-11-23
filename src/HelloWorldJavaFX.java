f3332094f2735e83eb5594f88214d001947c0dbc
Adrielle Costa <adrielle.gasparc@gmail.com>
2018-11-23 16:45:15 -0300

    Adicionando formas ao projeto

--- src/HelloWorldJavaFX.java
+++ src/HelloWorldJavaFX.java
@@ -1,7 +1,8 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class HelloWorldJavaFX extends Application{
@@ -11,12 +12,14 @@ public class HelloWorldJavaFX extends Application{
     @Override
     public void start(Stage palco){
         //Personagem
-        Label texto = new Label("Olá mudo!");
+        Rectangle ceu = new Rectangle(900, 450);
+        Rectangle mar = new Rectangle(900, 450);
+        Circle sol = new Circle(90);
         //Historia
         StackPane historia = new StackPane();
-        historia.getChildren().add(texto);
+        historia.getChildren().addAll(ceu, sol, mar);
         //Cena
-        Scene cena = new Scene(historia, 300,300);
+        Scene cena = new Scene(historia, 900,900);
         //Colocando a cena no palco
         palco.setScene(cena);
         palco.show();
