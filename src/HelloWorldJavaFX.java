c37c62d1837f246ff1b282a1ed6a978dfa0e2dd9
Adrielle Costa <adrielle.gasparc@gmail.com>
2018-11-23 18:05:13 -0300

    Algumas modificações em posicionamento

--- src/HelloWorldJavaFX.java
+++ src/HelloWorldJavaFX.java
@@ -23,17 +23,17 @@ public class HelloWorldJavaFX extends Application{
         AudioClip clip = new AudioClip(musica);
         clip.play();
         //Personagem
-        Rectangle ceu = new Rectangle(800, 450, Color.ORANGERED);
-        Rectangle mar = new Rectangle(800, 450, Color.DARKBLUE);
-        Rectangle ceu2 = new Rectangle(800, 450, Color.YELLOW);
-        Rectangle mar2 = new Rectangle(800, 450, Color.LIGHTBLUE);
+        Rectangle ceu = new Rectangle(600, 450, Color.ORANGERED);
+        Rectangle mar = new Rectangle(600, 450, Color.DARKBLUE);
+        Rectangle ceu2 = new Rectangle(600, 450, Color.YELLOW);
+        Rectangle mar2 = new Rectangle(600, 450, Color.LIGHTBLUE);
         Circle sol = new Circle(90, Color.YELLOW);
         //Posicionamento
         ceu2.setTranslateY(-225);
         mar2.setTranslateY(225);
         ceu.setTranslateY(-225);
         mar.setTranslateY(225);
-        sol.setTranslateY(0);      
+        sol.setTranslateY(100);      
         //Movimento
         TranslateTransition trtr = new TranslateTransition(Duration.millis(20000), sol);
             trtr.setByY(-330);
@@ -47,7 +47,7 @@ public class HelloWorldJavaFX extends Application{
             ft.setCycleCount(Timeline.INDEFINITE);
             ft.setAutoReverse(true);
             ft.play();
-        FadeTransition mt = new FadeTransition(Duration.millis(40000), mar);
+        FadeTransition mt = new FadeTransition(Duration.millis(20000), mar);
             mt.setFromValue(2.0);
             mt.setToValue(0.5);
             mt.setCycleCount(Timeline.INDEFINITE);
